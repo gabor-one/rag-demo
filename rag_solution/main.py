a27374663aa2d12ad32120d848b0079588553c56
Gabor Kiss <kiss.gabor.attila@gmail.com>
2025-07-29 21:36:58 +0200

    feat: added prometheus metrics exporter

--- rag_solution/main.py
+++ rag_solution/main.py
@@ -1,11 +1,14 @@
 from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from rag_solution.rate_limiter import setup_limiter
 from rag_solution.singleton_worker_pool import SingletonWorkerPool
-from .routers.health import router as health_router
+
 from .routers.documents import router as documents_router
+from .routers.health import router as health_router
 
 
 @asynccontextmanager
@@ -24,7 +27,14 @@ app = FastAPI(
     description="RAG Solution Homework for 21.co",
     lifespan=lifespan
 )
+
+# Setup rate limiter with global default
 setup_limiter(app)
+
+# Setup Prometheus instrumentation
+#   This will expose metrics on /metrics endpoint with default webserver metrics.
+#   Application specific custom metrics could be added like: vector DB response time etc.
+Instrumentator().instrument(app).expose(app, tags=["Metrics"])
 
 app.include_router(health_router)
 app.include_router(documents_router)
@@ -38,9 +48,9 @@ def dev():
 #   Use this for production deployments, e.g. with Docker or systemd
 #   It will not reload on code changes, which is more efficient for production use.
 #   This accepts all traffic.
-#   Gunicorn is recommended if vertical scaling is desired. Load bearing part is already scaling.
-#   My preffered way of scaling is horizontal scaling with multiple instances (e.g.: Kubernetes)...
-#     ... due to added extra functionalities: 
+#   Gunicorn is recommended if process level horizontal scaling is desired. Load bearing part is already vertically scaling with uvicorn.
+#   My preferred way of scaling is horizontal scaling with multiple instances (e.g.: Kubernetes)...
+#     ... due to added extra functionalities:
 #     zero-downtime updates (both app and platform), auto-scaling, use-what-you-need, etc.
 def serve():
     uvicorn.run("rag_solution.main:app", reload=False, host="0.0.0.0", log_level="info")
